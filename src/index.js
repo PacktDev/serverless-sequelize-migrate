8cbf546a5e4c1d4ee3771a59c326adb87413711d
Mike Elsmore <mike@elsmore.me>
2018-05-08 15:43:34 +0100

    Add prepublishing stage to enforce webpack

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Sequelize from'sequelize';
 
 export default class Migration {
   constructor(config) {
+    // Create Sequelize instance
     this.sequelize = new Sequelize(config.dbName, config.dbUser, config.dbPass, {
       host: config.dbHost,
       dialect: 'postgres'
     });
 
+    // Create Umzub instance
     this.umzug = new Umzug({
       storage: 'sequelize',
       storageOptions: {
@@ -19,6 +21,18 @@ export default class Migration {
         pattern: /\.js$/
       }
     });
+
+    // Check that the database exists
+    this.sequelize.query(`SELECT 1 FROM pg_catalog.pg_database WHERE datname = '${config.dbName}'`)
+    .then((database)=>{
+      if(!database[1].rowCount) {
+        return sequelize.query(`CREATE DATABASE ${queryInterface.quoteIdentifier(config.dbName)}`);
+      }
+      return Promise.resolve()
+    })
+    .then(()=>{
+      console.log(`Finished checking ${config.dbName} exists before migrating`)
+    })
   }
 
   up(event, context, callback) {
