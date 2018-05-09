4a753eeb21d2c68b0999da2a85747da2ab395e96
Mike Elsmore <mike@elsmore.me>
2018-05-09 11:49:26 +0100

    Return to a simpler time, before it had to create the DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Umzug from'umzug';
 import Sequelize from 'sequelize';
-import { Client } from 'pg';
 
 export default class Migration {
   constructor(config) {
@@ -29,81 +28,21 @@ export default class Migration {
   }
 
   up(event, context, callback) {
-    const config = this.config;
-    const umzug = this.umzug;
-
-    Promise.resolve()
-    .then(()=>{
-      // Create the DB if it doesn't exist
-      if(process.env.SLS_DEBUG) {
-        console.log('Check DB exists');
-        console.log('Create PG client');
-      }
-      const client = new Client({
-        host: config.dbHost,
-        port: config.dbPort || 5432,
-        user: config.dbUser,
-        password: config.dbPass,
-        database: 'postgres'
+    this.umzug
+      .up()
+      .then(() => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Migration Up Complete',
+            input: event
+          })
+        };
+        callback(null, response);
       })
-      return Promise.resolve()
-      .then(()=>{
-        if(process.env.SLS_DEBUG) {
-          console.log('Connect to PG');
-        }
-        return client.connect();
-      })
-      .then(()=>{
-        if(process.env.SLS_DEBUG) {
-          console.log(`Check if ${config.dbName} exists`);
-        }
-        const query = `SELECT datname FROM pg_catalog.pg_database WHERE datname = '${config.dbName}'`;
-        return client.query(query)
-      })
-      .then((data)=>{
-        if(!data.rowCount) {
-          if(process.env.SLS_DEBUG) {
-            console.log(`Create ${config.dbName}`);
-          }
-          const query = `CREATE DATABASE "${config.dbName}"`;
-          return client.query(query);
-        } else {
-          if(process.env.SLS_DEBUG) {
-            console.log(`${config.dbName} exists`);
-          }
-          return Promise.resolve()
-        }
-      })
-      .then(()=>{
-        if(process.env.SLS_DEBUG) {
-          console.log(`Close PG`);
-        }
-        client.end()
-      })
-      .catch((error)=>{
-        if(process.env.SLS_DEBUG) {
-          console.log('ERROR DURING DB CREATE')
-          console.log(error);
-        }
-        return Promise.resolve();
-      })
-    })
-    .then(()=>{
-      return umzug.up()
-        .then(() => {
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify({
-              message: 'Migration Up Complete',
-              input: event
-            })
-          };
-          callback(null, response);
-        })
-        .catch(error => {
-          callback(error, error);
-        });
-    })
+      .catch(error => {
+        callback(error, error);
+      });
   };
 
   down(event, context, callback) {
